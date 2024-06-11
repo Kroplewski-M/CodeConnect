66b26ebf8c24177cc60a9aa8d529489cf25a785c
Mateusz Kroplewski <kroplewskimateusz@gmail.com>
2024-06-11 21:25:42 +0100

    init

---
+++ TestLayer/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace TestLayer;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        //Arrange
+        int x = 5;
+        //Act
+        
+        //Assert
+        Assert.Equal(5,x);
+    }
+}
