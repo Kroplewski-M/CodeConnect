56513aad9ddf1fe90926853359d830db6b5c747e
Mateusz Kroplewski <kroplewskimateusz@gmail.com>
2024-06-16 21:45:31 +0100

    generating jwt token correct way

---
+++ CodeConnect.WebAPI/Endpoints/AuthenticationEndpoint/AuthenticationController.cs
@@ -0,0 +1,21 @@
+using ApplicationLayer.Interfaces;
+using DomainLayer.Entities.Auth;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodeConnect.WebAPI.Endpoints.AuthenticationEndpoint;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthenticationController(IAuthenticateService authenticateService) : ControllerBase
+{
+    [HttpPost("CreateUser")]
+    public async Task<IActionResult> CreateUser([FromBody]RegisterFormViewModel registerForm)
+    {
+        var result = await authenticateService.CreateUser(registerForm);
+        if (result.Flag )
+        {
+            return Ok(result);
+        }
+        return BadRequest("Account creation Failed");
+    }
+}
