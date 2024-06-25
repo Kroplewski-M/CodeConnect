64f1f70cf196d2c940476fe153c0b7aa5861dd56
Mateusz Kroplewski <kroplewskimateusz@gmail.com>
2024-06-25 21:42:36 +0100

    webassembly fixesd

--- CodeConnect.WebAssembly/Pages/HomeBase.cs
+++ CodeConnect.WebAssembly/Pages/HomeBase.cs
@@ -5,5 +5,17 @@ namespace CodeConnect.WebAssembly.Pages;
 
 public class HomeBase : ComponentBase
 {
- 
+    public string hello = string.Empty;
+    protected override async Task OnInitializedAsync()
+    {
+        try
+        {
+            HttpClient client = new HttpClient();
+            hello = await client.GetStringAsync("https://localhost:7124/api/HelloWorld");
+        }
+        catch (Exception err)
+        {
+            Console.Write(err);
+        }
+    }
 }
