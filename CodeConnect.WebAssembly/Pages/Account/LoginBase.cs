f77c37047e8d530507396b378d950053b41e183a
Mateusz Kroplewski <kroplewskimateusz@gmail.com>
2024-07-01 19:27:13 +0100

    added validation to auth pages

--- CodeConnect.WebAssembly/Pages/Account/LoginBase.cs
+++ CodeConnect.WebAssembly/Pages/Account/LoginBase.cs
@@ -1,6 +1,7 @@
 using System.Net.Http;
 using DomainLayer.Entities.Auth;
 using DomainLayer.Entities.Validators;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Components;
 
 namespace CodeConnect.WebAssembly.Pages.Account;
@@ -8,11 +9,22 @@ namespace CodeConnect.WebAssembly.Pages.Account;
 public class LoginBase : ComponentBase
 {
     public LoginForm LoginForm = new LoginForm();
+    public List<ValidationFailure> LoginErrors = new List<ValidationFailure>();
 
     public async Task SubmitLogin()
     {
         LoginFormValidator loginFormValidator = new LoginFormValidator();
-        var validate = loginFormValidator.Validate(LoginForm);
+        var validate = await loginFormValidator.ValidateAsync(LoginForm);
+        
+        if (!validate.IsValid)
+        {
+            LoginErrors = validate.Errors;
+            StateHasChanged();
+        }
+        else
+        {
+            LoginErrors = [];
+        }
     }
     
 }
