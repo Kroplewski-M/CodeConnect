01e0f5cfcf647815e3e4dc4047a5d59a570c8afd
Mateusz Kroplewski <kroplewskimateusz@gmail.com>
2024-07-09 20:24:12 +0100

    styled home page and moved notifications service

--- CodeConnect.WebAssembly/Pages/HomeBase.cs
+++ CodeConnect.WebAssembly/Pages/HomeBase.cs
@@ -1,6 +1,4 @@
-using System.Net.Http;
 using Microsoft.AspNetCore.Components;
-
 namespace CodeConnect.WebAssembly.Pages;
 
 public class HomeBase : ComponentBase
--- CodeConnect.WebAssembly/tailwind.config.js
+++ CodeConnect.WebAssembly/tailwind.config.js
@@ -12,10 +12,19 @@ module.exports = {
         },
         dark: {
           primaryColor: "#444444",
-          secondaryColor: "#5C42A7",
+          secondaryColor: "#7b62cc",
           supportColor: "#E8E7E7",
         }
-      }
+      },
+      keyframes:{
+        fadeIn:{
+          "0%": { opacity: "0", transform: "translateX(-10%)" },
+          "100%": { opacity: "1", transform: "translateX(0);" },
+        }
+      },
+      animation: {
+        fadeIn: "fadeIn 1s ease-in",
+      },
     },
   },
   plugins: [],
