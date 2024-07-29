e1cc2ddeae03109563886abec50e81d720de8e7b
Mateusz Kroplewski <kroplewskimateusz@gmail.com>
2024-07-29 21:45:48 +0100

    started edit profile page

--- CodeConnect.WebAssembly/Pages/Account/EditProfileBase.cs
+++ CodeConnect.WebAssembly/Pages/Account/EditProfileBase.cs
@@ -1,12 +1,48 @@
+using ApplicationLayer.ClientServices;
+using ApplicationLayer.DTO_s;
+using ApplicationLayer.Interfaces;
 using DomainLayer.Entities;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 
 namespace CodeConnect.WebAssembly.Pages.Account;
 
 public class EditProfileBase : ComponentBase
 {
-    protected EditProfileForm EditProfileForm = new EditProfileForm();
+    [Inject]
+    public required  IAuthenticateServiceClient AuthenticateServiceClient { get; set; }
     
+    protected EditProfileForm EditProfileForm = new EditProfileForm();
+    private UserDetails _userDetails;
+
+    [CascadingParameter]
+    private Task<AuthenticationState>? AuthenticationState { get; set; }
+    
+    protected override async Task OnInitializedAsync()
+    {
+        if (AuthenticationState is not null)
+        {
+            var authState = await AuthenticationState;
+            var user = authState?.User;
+
+            if (user?.Identity is not null && user.Identity.IsAuthenticated)
+            {
+                _userDetails = AuthenticateServiceClient.GetUserFromFromAuthState(authState);
+                EditProfileForm = new EditProfileForm
+                {
+                    FirstName = _userDetails.firstName,
+                    LastName = _userDetails.lastName,
+                    DOB = _userDetails.DOB,
+                    Bio = _userDetails.bio,
+                    ProfileImgUrl = "",
+                    ProfileImgExtention = "",
+                    BackgroundImgUrl = "",
+                    BackgoundImgExtention = ""
+                };
+                StateHasChanged();
+            }
+        }
+    }
     protected void ConfirmEditProfile()
     {
         
--- DomainLayer/Entities/EditProfileForm.cs
+++ DomainLayer/Entities/EditProfileForm.cs
@@ -4,9 +4,10 @@ public class EditProfileForm
 {
     public string FirstName { get; set; }
     public string LastName { get; set; }
-    public DateOnly DOB { get; set; }
+    public DateOnly? DOB { get; set; }
     public string ProfileImgUrl { get; set; }
     public string ProfileImgExtention { get; set; }
     public string BackgroundImgUrl { get; set; }
     public string BackgoundImgExtention { get; set; }
+    public string Bio { get; set; }
 }
