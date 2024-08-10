c694ccf215d85809802740427a5e5eb19a1e9753
Mateusz <kroplewskimateusz@gmail.com>
2024-08-10 11:21:19 +0100

    handler fetching token propely

--- CodeConnect.WebAssembly/DelegatingHandler/AuthHandler.cs
+++ CodeConnect.WebAssembly/DelegatingHandler/AuthHandler.cs
@@ -1,17 +1,18 @@
 using ApplicationLayer.Interfaces;
 using Blazored.LocalStorage;
+using DomainLayer.Constants;
 
 namespace CodeConnect.WebAssembly.DelegatingHandler;
 
 
-public class AuthHandler
-    : System.Net.Http.DelegatingHandler
+public class AuthHandler(ILocalStorageService localStorageService) : System.Net.Http.DelegatingHandler
 {
     
-    protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
     {
-        var response = base.SendAsync(request, cancellationToken);
-        Console.WriteLine("This is form the handler");
+        var response = await base.SendAsync(request, cancellationToken);
+        var token = await localStorageService.GetItemAsync<string>(Tokens.AuthToken);
+        Console.WriteLine("This is form the handler token: " + token);
         return response;
     }
 }
