e9f2d7369bf9a7ed8e3aa4eec7f94bf618b015d6
Mateusz <kroplewskimateusz@gmail.com>
2024-08-28 21:27:53 +0100

    started updating images

--- CodeConnect.WebAssembly/Components/UpdateImageBase.cs
+++ CodeConnect.WebAssembly/Components/UpdateImageBase.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
 
 namespace CodeConnect.WebAssembly.Components;
 
@@ -14,7 +15,26 @@ public class UpdateImageBase : ComponentBase
     public TypeOfUpdate UpdateOfType { get; set; }
     [Parameter]
     public EventCallback Cancel { get; set; }
-    
-    protected string typeOfUploadedImg { get; set; }
-    protected string UploadedImgBase64 { get; set; }
+
+    protected string TempFilePath { get; set; } = ""; 
+    protected async Task HandleFileSelection(InputFileChangeEventArgs e)
+    {
+        string result = Path.GetTempPath();
+        Console.WriteLine(result);
+        var file = e.GetMultipleFiles().FirstOrDefault();
+        if (file != null)
+        {
+            string tempFileName = "temp" + Path.GetExtension(file.Name);
+            TempFilePath = Path.Combine(Path.GetTempPath(), tempFileName);
+            await using (var stream = file.OpenReadStream())
+            {
+                await using (var fileStream = new FileStream(TempFilePath, FileMode.Create, FileAccess.ReadWrite))
+                {
+                    await stream.CopyToAsync(fileStream);
+                }
+            }
+            Console.WriteLine($"File saved to: {TempFilePath}");
+        }
+        StateHasChanged();
+    }
 }
