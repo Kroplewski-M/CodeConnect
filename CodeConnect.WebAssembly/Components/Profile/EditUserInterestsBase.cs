0d1a900d4fd72ff016e41b1a9b2ade503ecb5bc4
Mateusz <kroplewskimateusz@gmail.com>
2024-09-19 07:44:24 +0100

    started working on interests UI

--- CodeConnect.WebAssembly/Components/Profile/EditUserInterestsBase.cs
+++ CodeConnect.WebAssembly/Components/Profile/EditUserInterestsBase.cs
@@ -1,3 +1,7 @@
+using ApplicationLayer;
+using ApplicationLayer.APIServices;
+using ApplicationLayer.ClientServices;
+using ApplicationLayer.DTO_s;
 using Microsoft.AspNetCore.Components;
 
 namespace CodeConnect.WebAssembly.Components.Profile;
@@ -5,5 +9,29 @@ namespace CodeConnect.WebAssembly.Components.Profile;
 public class EditUserInterestsBase : ComponentBase
 {
     [Parameter]
-    public EventCallback Cancel { get; set; }   
+    public EventCallback Cancel { get; set; } 
+    [Parameter]
+    public UserInterestsDto? CurrentUserInterests { get; set; }
+    
+    [Inject]
+    public UserServiceClient UserService { get; set; }
+    [Inject]
+    public NotificationsService NotificationsService { get; set; }
+    public List<TechInterestsDto> AllTechInterests { get; set; }
+    public List<string>TechTypes { get; set; } = new List<string>();
+    
+    protected override async Task OnParametersSetAsync()
+    {
+        await base.OnParametersSetAsync();
+        try
+        {
+            AllTechInterests = await UserService.GetAllInterests();
+        }
+        catch
+        {
+            NotificationsService.PushNotification(new ApplicationLayer.Notification("Failed to fetch interests", NotificationType.Error));
+        }
+        TechTypes = AllTechInterests.Select(x=>x.InterestType).Distinct().ToList();
+    }
+    
 }
