a16ba3437c5c1e24b2d2aec2ee7891a60849c0da
Mateusz Kroplewski <kroplewskimateusz@gmail.com>
2024-10-07 19:17:40 +0100

    styled post component

--- CodeConnect.WebAssembly/Components/Feed/CreatePostBase.cs
+++ CodeConnect.WebAssembly/Components/Feed/CreatePostBase.cs
@@ -1,8 +1,35 @@
+using ApplicationLayer.ClientServices;
+using ApplicationLayer.DTO_s;
+using ApplicationLayer.Interfaces;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 
 namespace CodeConnect.WebAssembly.Components.Feed;
 
 public class CreatePostBase : ComponentBase
 {
-    
+    [CascadingParameter]
+    private Task<AuthenticationState>? AuthenticationState { get; set; }
+    [Inject]
+    IAuthenticateServiceClient AuthenticateServiceClient { get; set; } = null!;
+    protected UserDetails? UserDetails = null;
+
+    protected string PostContent { get; set; } = string.Empty;
+
+    protected override async Task OnInitializedAsync()
+    {
+        var authState = await AuthenticationState;
+        var user = authState?.User;
+
+        if (user?.Identity is not null && user.Identity.IsAuthenticated)
+        {
+            UserDetails = AuthenticateServiceClient.GetUserFromFromAuthState(authState);
+            await InvokeAsync(StateHasChanged);
+        }
+    }
+
+    protected void HandleValidSubmit()
+    {
+        Console.WriteLine("HandleValidSubmit");
+    }
 }
