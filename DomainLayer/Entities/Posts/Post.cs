2287993aae4905907f14b3cc5c0e5a9fd8542849
Mateusz Kroplewski <kroplewskimateusz@gmail.com>
2024-10-18 18:26:47 +0100

    removed file id

--- DomainLayer/Entities/Posts/Post.cs
+++ DomainLayer/Entities/Posts/Post.cs
@@ -6,7 +6,6 @@ public class Post
 {
     public int Id { get; set; }
     public required string Content { get; set; }
-    public string? FileId { get; set; }
     public DateTime CreatedAt { get; set; }
     public required string CreatedByUserId { get; set; }
     public required ApplicationUser CreatedByUser { get; set; }
