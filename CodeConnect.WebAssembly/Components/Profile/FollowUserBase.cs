fd3350b043ea5ed48b4b0b951de657baeb81d986
Mateusz Kroplewski <kroplewskimateusz@gmail.com>
2024-11-07 19:41:51 +0000

    created general confirm component

--- CodeConnect.WebAssembly/Components/Profile/FollowUserBase.cs
+++ CodeConnect.WebAssembly/Components/Profile/FollowUserBase.cs
@@ -6,30 +6,36 @@ namespace CodeConnect.WebAssembly.Components.Profile;
 
 public class FollowUserBase : ComponentBase
 {
-    [Inject]
-    public required IUserService UserService { get; set; } 
+    [Inject] public required IUserService UserService { get; set; }
     protected bool Following { get; set; } = false;
-    protected bool IsHovering { get; set; } = false;
-    [Parameter]
-    public required string CurrentUsername { get; set; }
-    [Parameter]
-    public required string FollowUsername { get; set; }
+    [Parameter] public required string CurrentUsername { get; set; }
+    [Parameter] public required string FollowUsername { get; set; }
 
-    protected void ToggleFollow()
+    protected async Task ToggleFollow()
     {
-        Console.WriteLine($"Current user: {CurrentUsername}");
-        Console.WriteLine($"Target user: {FollowUsername}");
-
         var request = new FollowRequestDto(CurrentUsername, FollowUsername);
-        if (Following)
+        if (!Following)
         {
-            UserService.FollowUser(request);
+            await UserService.FollowUser(request);
         }
-        else
-        {
-            UserService.UnfollowUser(request);
-        }
+
         Following = !Following;
-        StateHasChanged();
+        if (!Following)
+            ConfirmUnFollow = true;
+        await InvokeAsync(StateHasChanged);
     }
+
+    protected bool ConfirmUnFollow { get; set; } = false;
+
+    protected async Task UnFollow()
+    {
+        var request = new FollowRequestDto(CurrentUsername, FollowUsername);
+        await UserService.FollowUser(request);
+        Following = !Following;
+        await InvokeAsync(StateHasChanged);
+    }
+
+    protected void CancelUnFollow() => ConfirmUnFollow = false;
 }
+    
+    
