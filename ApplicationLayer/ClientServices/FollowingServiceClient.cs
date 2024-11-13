130189d892799bc9a109bf54d83be0efb6fca8ca
Mateusz Kroplewski <kroplewskimateusz@gmail.com>
2024-11-13 22:05:55 +0000

    added errors to following service

--- ApplicationLayer/ClientServices/FollowingServiceClient.cs
+++ ApplicationLayer/ClientServices/FollowingServiceClient.cs
@@ -4,13 +4,13 @@ using ApplicationLayer.Interfaces;
 
 namespace ApplicationLayer.ClientServices;
 
-public class FollowingServiceClient(HttpClient httpClient) : IFollowingService
+public class FollowingServiceClient(HttpClient httpClient, NotificationsService notificationsService) : IFollowingService
 {
     public async Task<FollowerCount> GetUserFollowers(string userName)
     {
         var response = await httpClient.GetFromJsonAsync<FollowerCount>($"api/Following/UserFollowing?username={userName}");
         if(response == null)
-            throw new UnauthorizedAccessException();
+            notificationsService.PushNotification(new Notification("An Error Occured when fetching followers", NotificationType.Error));
         return response;
     }
 
@@ -19,6 +19,7 @@ public class FollowingServiceClient(HttpClient httpClient) : IFollowingService
         var response = await httpClient.PostAsJsonAsync<FollowRequestDto>("api/Following/FollowUser", followRequest);
         if(response.IsSuccessStatusCode)
             return (await response.Content.ReadFromJsonAsync<ServiceResponse>())!;
+        notificationsService.PushNotification(new Notification("An Error Occured when following user", NotificationType.Error));
         return new ServiceResponse(false, "Error Occured");
     }
 
@@ -27,6 +28,7 @@ public class FollowingServiceClient(HttpClient httpClient) : IFollowingService
         var response = await httpClient.PostAsJsonAsync<FollowRequestDto>("api/Following/FollowUser",unFollowRequest);
         if(response.IsSuccessStatusCode)
             return (await response.Content.ReadFromJsonAsync<ServiceResponse>())!;
+        notificationsService.PushNotification(new Notification("An Error Occured when unfollowing user", NotificationType.Error));
         return new ServiceResponse(false, "Error Occured");
     }
 
