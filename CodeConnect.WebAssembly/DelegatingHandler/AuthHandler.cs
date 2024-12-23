7a0aba89967e3c3e3bfbda3fedb1f21cc0db46c7
Mateusz Kroplewski <kroplewskimateusz@gmail.com>
2024-12-23 21:23:13 +0000

    rewrote handler logic

--- CodeConnect.WebAssembly/DelegatingHandler/AuthHandler.cs
+++ CodeConnect.WebAssembly/DelegatingHandler/AuthHandler.cs
@@ -17,28 +17,38 @@ public class AuthHandler(ILocalStorageService localStorageService) : System.Net.
     protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
     {
         var token = await localStorageService.GetItemAsync<string>(Consts.Tokens.AuthToken, cancellationToken);
-        if (!string.IsNullOrEmpty(token))
+        var hasToken = !string.IsNullOrEmpty(token);
+        if (hasToken)
         {
             request.Headers.Authorization = new AuthenticationHeaderValue(Consts.Tokens.ApiAuthTokenName, token);
         }
-        var response = await base.SendAsync(request, cancellationToken);
-        if (response.StatusCode == HttpStatusCode.Unauthorized)
+        else
         {
-            var refreshToken = await localStorageService.GetItemAsync<string>(Consts.Tokens.RefreshToken, cancellationToken);
-            var refreshRequest = new HttpRequestMessage(HttpMethod.Get, $"{Consts.Base.BaseUrl}/api/Authentication/RefreshToken");
-            refreshRequest.Headers.Authorization = new AuthenticationHeaderValue(Consts.Tokens.ApiAuthTokenName, refreshToken);
-            var tokenResponse = await base.SendAsync(refreshRequest, cancellationToken);
-            var newToken = await tokenResponse.Content.ReadFromJsonAsync<AuthResponse>(cancellationToken: cancellationToken);
-            if (newToken?.Flag == true)
-            {
-                await localStorageService.SetItemAsync(Consts.Tokens.AuthToken,newToken.Token,cancellationToken);
-                await localStorageService.SetItemAsync(Consts.Tokens.RefreshToken,newToken.RefreshToken,cancellationToken);
-                request.Headers.Authorization = new AuthenticationHeaderValue(Consts.Tokens.ApiAuthTokenName, newToken.Token);
-                var retry = await base.SendAsync(request, cancellationToken);
-                return retry;
-            }
+            await RefreshTokenAndRetry(request, cancellationToken);
+        }
+        var response = await base.SendAsync(request, cancellationToken);
+        if (response.StatusCode == HttpStatusCode.Unauthorized && hasToken)
+        {
+            await RefreshTokenAndRetry(request, cancellationToken);
         }
         return response;
     }
-    
+
+    private async Task<HttpResponseMessage> RefreshTokenAndRetry(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        var refreshToken = await localStorageService.GetItemAsync<string>(Consts.Tokens.RefreshToken, cancellationToken);
+        var refreshRequest = new HttpRequestMessage(HttpMethod.Get, $"{Consts.Base.BaseUrl}/api/Authentication/RefreshToken");
+        refreshRequest.Headers.Authorization = new AuthenticationHeaderValue(Consts.Tokens.ApiAuthTokenName, refreshToken);
+        var tokenResponse = await base.SendAsync(refreshRequest, cancellationToken);
+        var newToken = await tokenResponse.Content.ReadFromJsonAsync<AuthResponse>(cancellationToken: cancellationToken);
+        if (newToken?.Flag == true)
+        {
+            await localStorageService.SetItemAsync(Consts.Tokens.AuthToken,newToken.Token,cancellationToken);
+            await localStorageService.SetItemAsync(Consts.Tokens.RefreshToken,newToken.RefreshToken,cancellationToken);
+            request.Headers.Authorization = new AuthenticationHeaderValue(Consts.Tokens.ApiAuthTokenName, newToken.Token);
+            var retry = await base.SendAsync(request, cancellationToken);
+            return retry;
+        }
+        return new HttpResponseMessage(HttpStatusCode.Unauthorized);
+    }
 }
