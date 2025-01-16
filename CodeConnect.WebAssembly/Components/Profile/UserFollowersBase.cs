bd1f986ced0d1a51a6cd91c6850cbe3bc3a03224
Mateusz Kroplewski <kroplewskimateusz@gmail.com>
2025-01-16 21:14:40 +0000

    added user basic component

--- CodeConnect.WebAssembly/Components/Profile/UserFollowersBase.cs
+++ CodeConnect.WebAssembly/Components/Profile/UserFollowersBase.cs
@@ -9,8 +9,11 @@ public class UserFollowersBase() : ComponentBase
     [Inject]
     public required IFollowingService FollowingService { get; set; }
     public List<UserBasicDto> Followers { get; set; } = new List<UserBasicDto>();
+
+    public bool Loading { get; set; } = true;
     protected override async Task OnInitializedAsync()
     {
         Followers = await FollowingService.GetUserFollowers(null);
+        Loading = false;
     }
 }
