0825488b3d7e7989c011d3988249ee98127a4872
Mateusz Kroplewski <kroplewskimateusz@gmail.com>
2025-04-16 18:09:19 +0100

    added next test file

---
+++ ApiServiceTests/AzureServiceTests.cs
@@ -0,0 +1,46 @@
+using System.Security.Authentication.ExtendedProtection;
+using ApplicationLayer.APIServices;
+using DomainLayer.Entities.APIClasses;
+using Microsoft.Extensions.Options;
+using Moq;
+using Azure;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using DomainLayer.Constants;
+
+namespace ApiServiceTests;
+
+public class AzureServiceTests
+{
+    private readonly Mock<IOptions<AzureSettings>> _azureSettingsMock;
+    private readonly Mock<BlobServiceClient> _blobServiceClient;
+    private readonly Mock<BlobContainerClient> _blobContainerClient;
+    private readonly Mock<BlobClient> _blobClient;
+    private readonly AzureService _azureService;
+
+    public AzureServiceTests()
+    {
+        _azureSettingsMock = new Mock<IOptions<AzureSettings>>();
+        _blobServiceClient = new Mock<BlobServiceClient>();
+        _blobContainerClient = new Mock<BlobContainerClient>();
+        _blobClient = new Mock<BlobClient>();
+        _azureService = new AzureService(_azureSettingsMock.Object);
+    }
+
+    [Fact]
+    public async Task UploadImage_ShouldReturnSuccess_WhenImageUploaded()
+    {
+        //Arrange
+        var imageType = Consts.ImageType.ProfileImages;
+        var base64Image = "base64Image";
+        var imageName = "imageName";
+        var imageExt = ".png";
+        // Act
+        var result = await _azureService.UploadImage(imageType, base64Image, imageName, imageExt);
+        //have to set up the private method
+        // Assert
+        Assert.True(result.Flag);
+        Assert.Equal("testimage.png", result.ImageName);
+        Assert.Equal("Image updated successfully", result.Message);
+    }
+}
