c1c727f1ab31867a9bf63e3e4aa26ab32b94b7bc
Mateusz Kroplewski <kroplewskimateusz@gmail.com>
2025-05-04 12:57:14 +0100

    minor changes

--- ApplicationLayer/DTO's/PostDto.cs
+++ ApplicationLayer/DTO's/PostDto.cs
@@ -1,3 +1,3 @@
 namespace ApplicationLayer.DTO_s;
 
-public record PostDto(int Id,string Content, List<string>ImageNames, List<CommentDto> Comments, int LikeCount);
+public record PostDto(int Id,string Content, List<string>ImageNames, string CreatedBy,DateTime CreatedAt, List<CommentDto> Comments, int LikeCount);
