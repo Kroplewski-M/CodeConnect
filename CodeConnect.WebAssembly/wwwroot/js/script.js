37a36db7dbec183739407e915137d0adddbce4be
Mateusz Kroplewski <kroplewskimateusz@gmail.com>
2025-05-30 17:07:34 +0100

    fixed create post text box resizing

--- CodeConnect.WebAssembly/wwwroot/js/script.js
+++ CodeConnect.WebAssembly/wwwroot/js/script.js
@@ -1,17 +1,20 @@
 window.toggleDarkMode = function(){
     document.body.classList.toggle('dark');
 }
-
+//#region create post textbox
 let lastHeight = 0;
+const minHeight = 50;
 function autoResizeTextAreaAndContainer(textarea) {
     //set to auto so it shrinks straight away when clearing
     textarea.style.height = 'auto';
-    if(textarea.scrollHeight > 50){
+    if(textarea.scrollHeight > minHeight){
         textarea.style.height = textarea.scrollHeight + 'px';
     }
     lastHeight = textarea.scrollHeight;
 }
 function postSizeOnBlur(elementId){
     let post = document.getElementById(elementId);
-    post.style.Height = lastHeight + 'px'; // Maintain last height if empty
+    if(lastHeight > minHeight)
+        post.style.height = lastHeight + 'px'; // Maintain last height if empty
 }
+//#endregion
