8894842be6aff41599152064f326880a6608b585
Mateusz Kroplewski <kroplewskimateusz@gmail.com>
2025-06-09 17:17:46 +0100

    using cascading parameter everywhere

--- CodeConnect.WebAssembly/Components/Profile/UserFollowersBase.cs
+++ CodeConnect.WebAssembly/Components/Profile/UserFollowersBase.cs
@@ -1,4 +1,5 @@
 using ApplicationLayer;
+using ApplicationLayer.Classes;
 using ApplicationLayer.DTO_s;
 using ApplicationLayer.DTO_s.User;
 using ApplicationLayer.Interfaces;
@@ -12,13 +13,14 @@ public class UserFollowersBase() : ComponentBase
     [Inject] public required IFollowingService FollowingService { get; set; }
     [Inject] public required NavigationManager NavigationManager { get; set; }
     [Inject] public required NotificationsService NotificationsService { get; set; }
-    [CascadingParameter] public required string Username { get; set; }
+    [CascadingParameter] public required UserState UserState { get; set; }
     public List<UserBasicDto> Followers { get; set; } = new List<UserBasicDto>();
     
     protected async Task LoadMoreFollowers((int,int)range)
     {
+        if(UserState.Current == null) return;
         var (startIndex, take) = range;
-        var more = await FollowingService.GetUserFollowers(Username, skip: startIndex, take: take);
+        var more = await FollowingService.GetUserFollowers(UserState.Current.UserName, skip: startIndex, take: take);
         if (more?.Any() == true)
         {
             Followers.AddRange(more);
--- CodeConnect.WebAssembly/Components/Profile/UserPostsBase.cs
+++ CodeConnect.WebAssembly/Components/Profile/UserPostsBase.cs
@@ -1,4 +1,5 @@
 using ApplicationLayer.APIServices;
+using ApplicationLayer.Classes;
 using ApplicationLayer.DTO_s;
 using ApplicationLayer.DTO_s.Post;
 using ApplicationLayer.Interfaces;
@@ -9,15 +10,16 @@ namespace CodeConnect.WebAssembly.Components.Profile;
 public class UserPostsBase : ComponentBase
 {
     [Inject] public required IPostService PostService { get; set; }
-    [CascadingParameter] public required string Username { get; set; }
+    [CascadingParameter] public required UserState UserState { get; set; }
     
     public List<PostBasicDto> Posts { get; set; } = new List<PostBasicDto>();
     public bool Loading { get; set; } = true;
 
     protected async Task LoadMorePosts((int,int)range)
     {
+        if(UserState.Current == null) return;
         var (startIndex, take) = range;
-        var more = await PostService.GetUserPosts(Username, skip: startIndex, take: take);
+        var more = await PostService.GetUserPosts(UserState.Current.UserName, skip: startIndex, take: take);
         if (more?.Any() == true)
         {
             Posts.AddRange(more);
