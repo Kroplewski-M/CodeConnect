67c59bc7124d04f764338dc57565c44f6f9bbcd1
Mateusz Kroplewski <kroplewskimateusz@gmail.com>
2025-06-19 18:29:44 +0100

    liking posts

--- CodeConnect.WebAssembly/Pages/Post/PostBase.cs
+++ CodeConnect.WebAssembly/Pages/Post/PostBase.cs
@@ -1,3 +1,4 @@
+using ApplicationLayer.Classes;
 using ApplicationLayer.DTO_s.Post;
 using ApplicationLayer.Interfaces;
 using Microsoft.AspNetCore.Components;
@@ -10,20 +11,38 @@ public class PostBase : ComponentBase
     [Parameter] public required Guid Id { get; set; }
     [Inject] public required IPostService PostService { get; set; }
     [Inject] public required IJSRuntime Js { get; set; }
+    [CascadingParameter] public required UserState UserState { get; set; }
     protected bool Loading { get; set; } = true;
     protected PostBasicDto? Post { get; set; }
+    protected bool IsUserLiking { get; set; }
+    protected int LikeCount { get; set; }
     protected override async Task OnParametersSetAsync()
     {
         SetLoading(true);
         Post = await PostService.GetPostById(Id);
+        LikeCount = Post?.LikeCount ?? 0;
+        IsUserLiking = await PostService.IsUserLikingPost(Id, UserState.Current?.UserName ?? "");
         SetLoading(false);
         await Js.InvokeVoidAsync("highlightCodeBlocks",Id.ToString());
-        
     }
     private void SetLoading(bool isLoading)
     {
         Loading = isLoading;
         StateHasChanged();
     }
-    
+    private bool LoadingLike { get; set; }
+    protected async Task ToggleLike()
+    {
+        if (LoadingLike)
+            return;
+        LoadingLike = true;
+        await PostService.ToggleLikePost(new LikePostDto(Id, UserState.Current?.UserName ?? ""));
+        IsUserLiking = !IsUserLiking;
+        if(IsUserLiking)
+            LikeCount++;
+        else
+            LikeCount--;
+        LoadingLike = false;
+        StateHasChanged();
+    }
 }
