a9bc1a3e6b5d4853b9de487bd9401b92b730084c
Mateusz Kroplewski <kroplewskimateusz@gmail.com>
2025-08-03 14:37:57 +0100

    added checks

--- SharedApplicationLayer/ExtensionClasses/ByteExtensions.cs
+++ SharedApplicationLayer/ExtensionClasses/ByteExtensions.cs
@@ -6,17 +6,26 @@ public static class ByteExtensions
 {
     public static byte[] RemoveMetaData(this byte[] bytes)
     {
-        var image = new MagickImage(bytes);
-        // Exif profile: Camera settings, GPS location, Date, Time Zone, ... etc
-        var exifProfile = image.GetExifProfile(); 
-        // IPTC profile: Copyright, Permissions and licenses, Creator's information and contact details, Rights usage terms, ...etc
-        var iptcProfile  = image.GetIptcProfile(); 
-        // Xmp profile: IPTC with additional information
-        var xmpProfile = image.GetXmpProfile();
-        
-        if(exifProfile != null) image.RemoveProfile(exifProfile);
-        if(iptcProfile != null) image.RemoveProfile(iptcProfile);
-        if(xmpProfile != null) image.RemoveProfile(xmpProfile);
-        return image.ToByteArray();
+        if (bytes.Length == 0)
+            return bytes;
+        try
+        {
+            var image = new MagickImage(bytes);
+            // Exif profile: Camera settings, GPS location, Date, Time Zone, ... etc
+            var exifProfile = image.GetExifProfile();
+            // IPTC profile: Copyright, Permissions and licenses, Creator's information and contact details, Rights usage terms, ...etc
+            var iptcProfile = image.GetIptcProfile();
+            // Xmp profile: IPTC with additional information
+            var xmpProfile = image.GetXmpProfile();
+
+            if (exifProfile != null) image.RemoveProfile(exifProfile);
+            if (iptcProfile != null) image.RemoveProfile(iptcProfile);
+            if (xmpProfile != null) image.RemoveProfile(xmpProfile);
+            return image.ToByteArray();
+        }
+        catch
+        {
+            return bytes;
+        }
     }
 }
