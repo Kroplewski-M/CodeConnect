a3854f0f575d2d727c5b936fc859030332042285
Mateusz Kroplewski <kroplewskimateusz@gmail.com>
2025-09-07 19:45:56 +0100

    updated post styling and added redirect on home page if the user is logged in

--- CodeConnect.WebAssembly/Pages/HomeBase.cs
+++ CodeConnect.WebAssembly/Pages/HomeBase.cs
@@ -1,7 +1,15 @@
+using ApplicationLayer.Classes;
 using Microsoft.AspNetCore.Components;
 namespace CodeConnect.WebAssembly.Pages;
 
 public class HomeBase : ComponentBase
 {
- 
+    [Inject] public required NavigationManager NavigationManager { get; set; } = null!;
+    [CascadingParameter] public required UserState UserState { get; set; }
+
+    protected override void OnInitialized()
+    {
+        if(UserState?.Current != null)
+            NavigationManager.NavigateTo("/MyFeed");
+    }
 }
